0603e6c072c805968e56972fec7017a33adeb406
TheDeadcoder <sakibbuet2k19@gmail.com>
2024-10-11 17:25:42 +0600

    get review BUG fixed

--- app/db/base.py
+++ app/db/base.py
@@ -4,3 +4,4 @@ from app.db.base_class import Base
 from app.db.models.users import User
 from app.db.models.products import Product
 from app.db.models.cart import Cart
+from app.db.models.reviews import Review
